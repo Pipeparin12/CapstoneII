89d129e27c7fdeb822eba935f89ca76a9ea93522
Pipeparin12 <79441231+Pipeparin12@users.noreply.github.com>
2023-10-12 15:19:21 +0700

    auth success

--- backend/src/interface/models/User.ts
+++ backend/src/interface/models/User.ts
@@ -3,7 +3,7 @@ import { UserType } from "@/database/models/schema";
 export default interface User {
 	username: string;
 	password: string;
-	email: string;
-	tel: string;
+	// email: string;
+	// tel: string;
 	type: UserType;
 }
