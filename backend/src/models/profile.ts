43d8db1c34987f7189c23ed70da3bd26939c719f
JPthanasan <jackpot2545@gmail.com>
2023-10-15 17:53:34 +0700

    complete sign up sign in

--- backend/src/models/profile.ts
+++ backend/src/models/profile.ts
@@ -19,10 +19,6 @@ export const ProfileSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    password: {
-        type: String,
-        required: true
-    },
     email: {
         type: String,
         required: true
--- longdoo_frontend/lib/service/api/user.dart
+++ longdoo_frontend/lib/service/api/user.dart
@@ -2,13 +2,19 @@ import 'package:longdoo_frontend/service/dio.dart';
 
 class UserApi {
   static Future<dynamic> signIn(String username, String password) async {
-    var response = await DioInstance.dio
-        .post("/auth/signin", data: {"username": username, "password": password});
+    var response = await DioInstance.dio.post("/auth/signin",
+        data: {"username": username, "password": password});
     return response;
   }
 
-  static Future<dynamic> signUp(String username, String password, String firstName, String lastName) async {
-    var response = await DioInstance.dio
-        .post('/auth/signup', data: {"username": username, "password": password, "firstName": firstName, "lastName": lastName});
+  static Future<dynamic> signUp(String username, String password,
+      String firstName, String lastName) async {
+    var response = await DioInstance.dio.post('/auth/signup', data: {
+      "username": username,
+      "password": password,
+      "firstName": firstName,
+      "lastName": lastName
+    });
+    return response;
   }
 }
