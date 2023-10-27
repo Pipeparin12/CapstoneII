d6269fcdbefe3773ab14ede2b6bc49cb364900ca
Pipeparin12 <79441231+Pipeparin12@users.noreply.github.com>
2023-10-27 15:20:37 +0700

    .

--- backend/src/routes/password.ts
+++ backend/src/routes/password.ts
@@ -5,20 +5,24 @@ import Profile from "@/models/profile";
 const newpassRoute = express.Router();
 
 
-newpassRoute.patch('/changepassword', async (req, res) => {
-    const { userId, currentPassword, newPassword } = req.body;
+newpassRoute.patch('/change-password', async (req, res) => {
+  if(req.user == null) return res.json({
+    success: false,
+    message: "Authentication error!"
+  });
 
-    // Check if the current password matches the password in the database.
-    const user = await User.findById(userId);
-    if (!user || user.password !== currentPassword) {
-      return res.status(400).json({ message: 'Incorrect current password.' });
-    }
+  const userId = req.user?.user_id;
+    const { currentPassword, newPassword } = req.body;
 
-    // Update the password in the database for both the users and profiles tables.
-    await User.findByIdAndUpdate(userId, { password: newPassword });
-
-    // Return a success response to the client.
-    return res.status(200).json({ message: 'Password changed successfully.' });
+  // Check if the current password matches the password in the database.
+  const user = await User.findById(userId);
+  if (!user || user.password !== currentPassword) {
+    return res.status(400).json({ message: 'Incorrect current password.' });
+  }
+  // Update the password in the database for both the users and profiles tables.
+  await User.findByIdAndUpdate(userId, { password: newPassword });
+  // Return a success response to the client.
+  return res.status(200).json({ message: 'Password changed successfully.' });
 });
 
 export default newpassRoute;
